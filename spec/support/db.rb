315a33a12b2f39694b70bfb6b661d613569b59ca
Valery <walerka100@yandex.ru>
2020-10-28 11:57:35 +0300

    add logging to tests involving the DB

--- spec/support/db.rb
+++ spec/support/db.rb
@@ -1,11 +1,20 @@
+require 'logger'
+
 RSpec.configure do |c|
   c.before(:suite) do
     Sequel.extension :migration
     Sequel::Migrator.run(DB, 'db/migrations')
     DB[:expenses].truncate
+
+    FileUtils.mkdir_p('log')
+    DB.loggers << Logger.new('log/sequel.log')
   end
 
   c.around(:example, :db) do |example|
+    DB.log_info("Starting example: #{example.full_description}")
+
     DB.transaction(rollback: :always) { example.run }
+
+    DB.log_info("Ending example: #{example.full_description}")
   end
 end
